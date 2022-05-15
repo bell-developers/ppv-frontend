485fa35b4735c85c08a629b0739a34810fee08b8
bell-developers <bell.developers.department@gmail.com>
2022-05-15 20:05:43 -0300

    Poppins setted as default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,18 @@ import 'styles/reset.css';
 import 'styles/base.css';
 import { Provider } from 'styletron-react';
 import { Client, Server } from 'styletron-engine-atomic';
-import { BaseProvider, LightTheme } from 'baseui';
+import { BaseProvider, LightTheme, createTheme } from 'baseui';
+import type { Theme } from 'baseui';
 
 const styletron = typeof window === 'undefined' ? new Server() : new Client();
+const customTheme: Theme = createTheme({
+    primaryFontFamily: 'Poppins',
+});
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <Provider value={styletron}>
-            <BaseProvider theme={LightTheme}>
+            <BaseProvider theme={customTheme}>
                 <Component {...pageProps} />
             </BaseProvider>
         </Provider>
