ce07af1c176259995e7a19fd50d7414e3ba48112
bell-developers <bell.developers.department@gmail.com>
2022-05-15 13:09:52 -0300

    Providers & HomePage's Header added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from 'next/app';
 import 'styles/reset.css';
 import 'styles/base.css';
+import { Provider } from 'styletron-react';
+import { Client, Server } from 'styletron-engine-atomic';
+import { BaseProvider, LightTheme } from 'baseui';
+
+const styletron = typeof window === 'undefined' ? new Server() : new Client();
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <Provider value={styletron}>
+            <BaseProvider theme={LightTheme}>
+                <Component {...pageProps} />
+            </BaseProvider>
+        </Provider>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
 import { NextPage } from 'next';
-import React from 'react';
+import {
+    ALIGN,
+    HeaderNavigation,
+    StyledNavigationItem,
+    StyledNavigationList,
+} from 'baseui/header-navigation';
+import { LabelLarge } from 'baseui/typography';
 
 const Home: NextPage = () => {
-    return <div>Home</div>;
+    return (
+        //@ts-ignore
+        <HeaderNavigation
+            overrides={{
+                Root: {
+                    style: {
+                        border: '1px solid transparent',
+                        paddingLeft: '5em',
+                    },
+                },
+            }}
+        >
+            <StyledNavigationList $align={ALIGN.left}>
+                <StyledNavigationItem>
+                    <LabelLarge>Logo</LabelLarge>
+                </StyledNavigationItem>
+            </StyledNavigationList>
+        </HeaderNavigation>
+    );
 };
 
 export default Home;
