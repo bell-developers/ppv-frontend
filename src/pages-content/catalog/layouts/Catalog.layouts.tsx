77e0a2c0c8585186d70e113decabfd5fbf3aac8d
bell-developers <bell.developers.department@gmail.com>
2022-05-29 16:24:57 -0300

    Setted responsive paddings and font-size

--- src/pages-content/catalog/layouts/Catalog.layouts.tsx
+++ src/pages-content/catalog/layouts/Catalog.layouts.tsx
@@ -8,30 +8,42 @@ export const CatalogBody = styled('div', ({ $theme }) => ({
     gap: '3.5em',
     maxWidth: '100vw',
     padding: '0 10em 3.5em 10em',
+    fontSize: 'clamp(16px, 1.2vw, 24px)',
     backgroundColor: $theme.colors.backgroundPrimary,
     [$theme.mediaQuery.medium]: {
-        padding: '0 4em',
+        padding: '0 4em 3.5em 4em',
+    },
+    [$theme.mediaQuery.small]: {
+        padding: '0 1em 3.5em 1em',
+        gap: '2.5em',
+    },
+}));
+
+export const CatalogTitleContainer = styled('div', ({ $theme }) => ({
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    maxWidth: '1250px',
+    [$theme.mediaQuery.small]: {
+        justifyContent: 'center',
+    },
+}));
+
+export const CatalogProductsContainer = styled('div', ({ $theme }) => ({
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(310px, 1fr))',
+    gridAutoRows: 'auto',
+    gap: '2em',
+    width: '100%',
+    maxWidth: '1250px',
+    boxSizing: 'border-box',
+    [$theme.mediaQuery.medium]: {
+        gridTemplateColumns: 'repeat(auto-fill, minmax(260px, 1fr))',
     },
     [$theme.mediaQuery.small]: {
         padding: '0 1em',
     },
 }));
-
-export const CatalogTitleContainer = styled('div', {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%',
-    maxWidth: '1250px',
-});
-
-export const CatalogProductsContainer = styled('div', {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(19.875em, 1fr))',
-    gridAutoRows: 'auto',
-    gap: '2em',
-    width: '100%',
-    maxWidth: '1250px',
-});
 
 export const CatalogProduct = () => {
     const [css, theme] = useStyletron();
