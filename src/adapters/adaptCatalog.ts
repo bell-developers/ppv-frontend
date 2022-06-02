6f4195a4b0ec29e7aa07bb3adeb92fef949e303b
bell-developers <bell.developers.department@gmail.com>
2022-06-02 20:38:01 -0300

    Added product's image adapter

--- src/adapters/adaptCatalog.ts
+++ src/adapters/adaptCatalog.ts
@@ -1,10 +1,14 @@
 import { Product } from 'models/Product.model';
+import { convertBase64ToObjectURL } from 'utils/convertBase64ToObjectURL';
+
+const adaptImages = (images: any[]) =>
+    images.map(image => convertBase64ToObjectURL(image, 'image/jpg'));
 
 export const adaptCatalog = (dataArray: any): Product[] => {
     return dataArray.map((data: any) => ({
         name: data.name,
         price: data.price,
-        images: data.images,
+        images: adaptImages(data.image),
         sizes: data.sizes,
     }));
 };
