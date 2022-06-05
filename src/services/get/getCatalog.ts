d5a8f2be2813d8739be13f268d8635dc692ac910
bell-developers <bell.developers.department@gmail.com>
2022-06-05 14:21:52 -0300

    Added correct URL to getCatalog service

--- src/services/get/getCatalog.ts
+++ src/services/get/getCatalog.ts
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
-export const getCatalog = async () => await axios.get('http://localhost:4000/catalog');
+export const getCatalog = async () =>
+    (await axios.get('http://127.0.0.1:4000/catalog')).data;
