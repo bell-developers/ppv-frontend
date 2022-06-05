5044ecad27e2997e2ebf9d50c6ed860f134235b6
bell-developers <bell.developers.department@gmail.com>
2022-06-05 14:20:56 -0300

    Removed base64ToBlob from adaptCatalog

--- src/adapters/adaptCatalog.ts
+++ src/adapters/adaptCatalog.ts
@@ -1,14 +1,11 @@
 import { Product } from 'models/Product.model';
-import { convertBase64ToObjectURL } from 'utils/convertBase64ToObjectURL';
-
-const adaptImages = (images: any[]) =>
-    images.map(image => convertBase64ToObjectURL(image, 'image/jpg'));
 
 export const adaptCatalog = (dataArray: any): Product[] => {
     return dataArray.map((data: any) => ({
+        id: data.id,
         name: data.name,
         price: data.price,
-        images: adaptImages(data.image),
+        images: data.images,
         sizes: data.sizes,
     }));
 };
