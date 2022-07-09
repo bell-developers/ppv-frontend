1a671552cc24cf9d81baf89f745042bda73f24c9
Luciano Aieta <lgaieta.dev@gmail.com>
2022-07-09 15:37:39 -0300

    Added padding to ProductPageBody

--- src/pages-content/product/layouts/ProductPage.layout.ts
+++ src/pages-content/product/layouts/ProductPage.layout.ts
@@ -1,7 +1,7 @@
 import { styled } from 'styletron-react';
 
 export const ProductPageBody = styled('div', {
+    padding: '0 1em',
     maxWidth: '100vw',
     minHeight: '100vh',
-    backgroundColor: 'red',
 });
