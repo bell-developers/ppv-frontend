770da158f08ac6b29cb5d510f37f32db8abd029b
Luciano Aieta <lgaieta.dev@gmail.com>
2022-07-21 20:23:04 -0300

    Replaced styled() with css() in BenefitsSectionLayout

---
+++ src/pages-content/home/layouts/BenefitsSection.layouts.tsx
@@ -0,0 +1,143 @@
+import { useStyletron } from 'baseui';
+import { DisplaySmall, HeadingXLarge, ParagraphLarge } from 'baseui/typography';
+import Image from 'next/image';
+
+const BenefitsSectionLayout = () => {
+    const [css, theme] = useStyletron();
+
+    return (
+        <div
+            className={css({
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '5em',
+                padding: '5em 0',
+                width: `min(${theme.custom.pageMaxWidth}, 100%)`,
+                fontSize: 'clamp(12px, 1.2vw, 20px)',
+            })}
+        >
+            <div
+                className={css({
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '1.5em',
+                    textAlign: 'center',
+                    width: '100%',
+                })}
+            >
+                <DisplaySmall
+                    as='h3'
+                    overrides={{
+                        Block: {
+                            style: ({ $theme }) => ({
+                                fontWeight: 600,
+                                color: $theme.colors.contentPrimary,
+                            }),
+                        },
+                    }}
+                >
+                    ¡Animate a comprar por internet!
+                </DisplaySmall>
+            </div>
+            <div
+                className={css({
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    gap: '6.5em',
+                    padding: '5em 6.5em',
+                    backgroundColor: theme.colors.backgroundSecondary,
+                    [theme.mediaQuery.medium]: {
+                        padding: '3vh 6vw',
+                        gap: '5vw',
+                    },
+                    [theme.mediaQuery.small]: {
+                        flexDirection: 'column-reverse',
+                    },
+                })}
+            >
+                <div
+                    className={css({
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '1.5em',
+                        width: '100%',
+                    })}
+                >
+                    <HeadingXLarge as='h4'>Comprá desde tu casa</HeadingXLarge>
+                    <ParagraphLarge>
+                        Ya no es necesario venir a nuestra tienda, podés hacerlo desde la
+                        comodidad de tu hogar.
+                    </ParagraphLarge>
+                </div>
+                <div
+                    className={css({
+                        width: '100%',
+                        position: 'relative',
+                        aspectRatio: '1.3 / 1',
+                        borderRadius: theme.borders.radius300,
+                    })}
+                >
+                    <Image
+                        src='/sofa.jpg'
+                        layout='fill'
+                        alt='Comprá desde tu casa'
+                        style={{ borderRadius: '8px' }}
+                    />
+                </div>
+            </div>
+            <div
+                className={css({
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    gap: '6.5em',
+                    padding: '5em 6.5em',
+                    backgroundColor: theme.colors.backgroundSecondary,
+                    flexDirection: 'row-reverse',
+                    [theme.mediaQuery.medium]: {
+                        padding: '3vh 6vw',
+                        gap: '5vw',
+                    },
+                    [theme.mediaQuery.small]: {
+                        flexDirection: 'column-reverse',
+                    },
+                })}
+            >
+                <div
+                    className={css({
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '1.5em',
+                        width: '100%',
+                    })}
+                >
+                    <HeadingXLarge as='h4'>
+                        Utilizá la forma de pago que más te guste
+                    </HeadingXLarge>
+                    <ParagraphLarge>
+                        Ofrecemos varias formas de pago para que elijas la que más te
+                        convenga.
+                    </ParagraphLarge>
+                </div>
+                <div
+                    className={css({
+                        width: '100%',
+                        position: 'relative',
+                        aspectRatio: '1.3 / 1',
+                        borderRadius: theme.borders.radius300,
+                    })}
+                >
+                    <Image
+                        src='/pay.jpg'
+                        layout='fill'
+                        alt='Utilizá la forma de pago que más te guste'
+                        style={{ borderRadius: '8px' }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BenefitsSectionLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,8 @@
 import { NextPage } from 'next';
-import { DisplaySmall, HeadingXLarge, ParagraphLarge } from 'baseui/typography';
 import { HomeBody } from 'pages-content/home/layouts/Home.layouts';
-import {
-    BenefitContainer,
-    BenefitContainerReversed,
-    BenefitContent,
-    BenefitImageContainer,
-    BenefitsSection,
-    BenefitsTitleContainer,
-    BenefitsTitleOverride,
-} from 'pages-content/home/layouts/BenefitsSection.layouts';
+import BenefitsSectionLayout from 'pages-content/home/layouts/BenefitsSection.layouts';
 import Header from 'components/header/Header';
 import Head from 'next/head';
-import Image from 'next/image';
 import { ValueProposalLayout } from 'pages-content/home/layouts/ValueProposalSection.layouts';
 
 const HomePage: NextPage = () => {
@@ -23,49 +13,7 @@ const HomePage: NextPage = () => {
             </Head>
             <Header />
             <ValueProposalLayout />
-            <BenefitsSection>
-                <BenefitsTitleContainer>
-                    <DisplaySmall as='h3' overrides={BenefitsTitleOverride}>
-                        ¡Animate a comprar por internet!
-                    </DisplaySmall>
-                </BenefitsTitleContainer>
-                <BenefitContainer>
-                    <BenefitContent>
-                        <HeadingXLarge as='h4'>Comprá desde tu casa</HeadingXLarge>
-                        <ParagraphLarge>
-                            Ya no es necesario venir a nuestra tienda, podés hacerlo desde
-                            la comodidad de tu hogar.
-                        </ParagraphLarge>
-                    </BenefitContent>
-                    <BenefitImageContainer>
-                        <Image
-                            src='/sofa.jpg'
-                            layout='fill'
-                            alt='Comprá desde tu casa'
-                            style={{ borderRadius: '8px' }}
-                        />
-                    </BenefitImageContainer>
-                </BenefitContainer>
-                <BenefitContainerReversed>
-                    <BenefitContent>
-                        <HeadingXLarge as='h4'>
-                            Utilizá la forma de pago que más te guste
-                        </HeadingXLarge>
-                        <ParagraphLarge>
-                            Ofrecemos varias formas de pago para que elijas la que más te
-                            convenga.
-                        </ParagraphLarge>
-                    </BenefitContent>
-                    <BenefitImageContainer>
-                        <Image
-                            src='/pay.jpg'
-                            layout='fill'
-                            alt='Utilizá la forma de pago que más te guste'
-                            style={{ borderRadius: '8px' }}
-                        />
-                    </BenefitImageContainer>
-                </BenefitContainerReversed>
-            </BenefitsSection>
+            <BenefitsSectionLayout />
         </HomeBody>
     );
 };
