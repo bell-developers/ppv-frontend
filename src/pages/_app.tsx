806fedda8c0e2c102d22e0d71b8f4c035ebebf64
Luciano Aieta <lgaieta.dev@gmail.com>
2022-07-21 19:40:19 -0300

    Moved CustomTheme.ts to /themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import 'styles/reset.css';
 import { Provider } from 'styletron-react';
 import { BaseProvider } from 'baseui';
-import { CustomTheme } from 'styles/CustomTheme';
+import { CustomTheme } from 'themes/CustomTheme';
 import { styletron } from 'utils/styletron';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
