848ab48c5375d5c0ec1def62c5c265d7b430c66e
Luciano Aieta <lgaieta.dev@gmail.com>
2022-07-24 13:27:39 -0300

    Replaced padding with pageMaxWidth in Catalog page

--- src/pages-content/catalog/layouts/Catalog.layouts.tsx
+++ src/pages-content/catalog/layouts/Catalog.layouts.tsx
@@ -8,7 +8,7 @@ export const CatalogBody = styled('div', ({ $theme }) => ({
     gap: '3.5em',
     maxWidth: '100vw',
     minHeight: '100vh',
-    padding: '0 10em 3.5em 10em',
+    padding: '0 1em 3.5em 1em',
     fontSize: 'clamp(16px, 1.2vw, 24px)',
     backgroundColor: $theme.colors.backgroundPrimary,
     [$theme.mediaQuery.medium]: {
@@ -23,8 +23,7 @@ export const CatalogBody = styled('div', ({ $theme }) => ({
 export const CatalogTitleContainer = styled('div', ({ $theme }) => ({
     display: 'flex',
     justifyContent: 'space-between',
-    width: '100%',
-    maxWidth: '1250px',
+    width: `min(${$theme.custom.pageMaxWidth}, 100%)`,
     color: $theme.colors.contentPrimary,
     [$theme.mediaQuery.small]: {
         justifyContent: 'center',
@@ -36,8 +35,7 @@ export const CatalogProductsContainer = styled('div', ({ $theme }) => ({
     gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
     gridAutoRows: 'auto',
     gap: '2em',
-    width: '100%',
-    maxWidth: '1250px',
+    width: `min(${$theme.custom.pageMaxWidth}, 100%)`,
     boxSizing: 'border-box',
     [$theme.mediaQuery.medium]: {
         gridTemplateColumns: 'repeat(auto-fill, minmax(260px, 1fr))',
@@ -59,16 +57,17 @@ export const CatalogSpinner = withStyle(Spinner, ({ $theme }) => ({
     height: '4em',
 }));
 
-export const CatalogErrorContainer = styled('div', () => ({
+export const CatalogErrorContainer = styled('div', ({ $theme }) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     gap: '1em',
-    textAlign: 'center',
     position: 'absolute',
     top: '50%',
     left: '0',
     right: '0',
-    transform: 'translateY(-50%)',
     margin: '0 auto',
+    maxWidth: $theme.custom.pageMaxWidth,
+    transform: 'translateY(-50%)',
+    textAlign: 'center',
 }));
