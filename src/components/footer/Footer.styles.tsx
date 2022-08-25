ebec7106951846a6db920cc8e81fda38633aebbc
lgaieta <lgaieta.dev@gmail.com>
2022-08-25 10:15:13 -0300

    Added styles and content to Footer (without icons)

--- src/components/footer/Footer.styles.tsx
+++ src/components/footer/Footer.styles.tsx
@@ -1,7 +1,27 @@
 import themedStyled from 'themes/utils/themedStyled';
 
 export const FooterContainer = themedStyled('div', ({ $theme }) => ({
+    display: 'block',
     width: '100%',
-    height: '10rem',
-    backgroundColor: $theme.colors.accent,
+    paddingBlock: '4rem',
+    backgroundColor: $theme.colors.backgroundSecondary,
 }));
+
+export const FooterItem = themedStyled('div', () => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+export const FooterNav = themedStyled('nav', () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '.5rem',
+    textAlign: 'center',
+}));
+
+export const FooterSocialMedia = themedStyled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '.5rem',
+});
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,46 @@
-import { FooterContainer } from './Footer.styles';
+import { Cell, Grid } from 'baseui/layout-grid';
+import { HeadingSmall } from 'baseui/typography';
+import {
+    FooterContainer,
+    FooterItem,
+    FooterNav,
+    FooterSocialMedia,
+} from './Footer.styles';
+import { StyledLink } from 'baseui/link';
 
 function Footer() {
-    return <FooterContainer>Footer</FooterContainer>;
+    return (
+        <FooterContainer>
+            <Grid
+                gridMaxWidth={850}
+                gridMargins={0}
+                gridGutters={0}
+                gridGaps={[32, 32, 0]}
+            >
+                <Cell span={[4, 8, 4]}>
+                    <FooterItem>
+                        <HeadingSmall>Bell</HeadingSmall>
+                    </FooterItem>
+                </Cell>
+                <Cell span={[4, 8, 4]}>
+                    <FooterItem>
+                        <FooterNav>
+                            <StyledLink href='/'>Inicio</StyledLink>
+                            <StyledLink href='/catalogo'>Catálogo</StyledLink>
+                        </FooterNav>
+                    </FooterItem>
+                </Cell>
+                <Cell span={[4, 8, 4]}>
+                    <FooterItem>
+                        <FooterSocialMedia>
+                            <div>Mandanos un mensaje</div>
+                            <div>@puraserendipia_</div>
+                        </FooterSocialMedia>
+                    </FooterItem>
+                </Cell>
+            </Grid>
+        </FooterContainer>
+    );
 }
 
 export default Footer;
