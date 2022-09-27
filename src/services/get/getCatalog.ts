beb1293f2fcda0918c296abd7325cdf9ca350642
lgaieta <lgaieta.dev@gmail.com>
2022-09-26 21:30:36 -0300

    Removed console log from getCatalog

--- src/services/get/getCatalog.ts
+++ src/services/get/getCatalog.ts
@@ -5,6 +5,5 @@ import axios from 'axios';
 const ApiURL = process.env.NEXT_PUBLIC_API_URL + '/catalog/20';
 
 export const getCatalog = async () => {
-    console.log(process.env.NEXT_PUBLIC_API_URL);
     return (await axios.get(ApiURL)).data;
 };
