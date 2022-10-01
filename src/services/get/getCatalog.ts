cf088973cef73acb24b0928de8fe5f1c1e98a99f
lgaieta <lgaieta.dev@gmail.com>
2022-10-01 16:28:12 -0300

    Changed API route in getCatalog

--- src/services/get/getCatalog.ts
+++ src/services/get/getCatalog.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const isDevelopmentEnv = process.env.NODE_ENV === 'development';
 
-const ApiURL = process.env.NEXT_PUBLIC_API_URL + '/catalog/20';
+const ApiURL = process.env.NEXT_PUBLIC_API_URL + '/catalog';
 
 export const getCatalog = async () => {
     return (await axios.get(ApiURL)).data;
