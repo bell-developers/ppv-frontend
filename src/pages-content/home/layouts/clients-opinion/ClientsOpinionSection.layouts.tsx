686c8ace7dba71d278ae62fa7908a227740c04ab
lgaieta <lgaieta.dev@gmail.com>
2022-10-01 16:28:34 -0300

    Added client cards in ClientsOpinionSection

--- src/pages-content/home/layouts/clients-opinion/ClientsOpinionSection.layouts.tsx
+++ src/pages-content/home/layouts/clients-opinion/ClientsOpinionSection.layouts.tsx
@@ -1,11 +1,19 @@
-import { HeadingLarge, LabelLarge } from 'baseui/typography';
+import { HeadingLarge, LabelLarge, LabelSmall } from 'baseui/typography';
+import Image from 'next/image';
+import themedUseStyletron from 'themes/utils/themedUseStyletron';
 import {
+    ClientsOpinionSectionCard,
+    ClientsOpinionSectionCardImageContainer,
+    ClientsOpinionSectionCardImageStyles,
+    ClientsOpinionSectionCardsContainer,
     ClientsOpinionSectionContainer,
     ClientsOpinionSectionHeadingStyles,
     ClientsOpinionSectionSubHeadingStyles,
 } from './ClientsOpinionSection.styles';
 
 function ClientsOpinionSectionLayout() {
+    const [css] = themedUseStyletron();
+
     return (
         <ClientsOpinionSectionContainer>
             <HeadingLarge
@@ -26,6 +34,50 @@ function ClientsOpinionSectionLayout() {
             >
                 ¡Estamos agradecidos con ustedes por confiar en nuestro emprendimiento!
             </LabelLarge>
+            <ClientsOpinionSectionCardsContainer>
+                <ClientsOpinionSectionCard>
+                    <ClientsOpinionSectionCardImageContainer>
+                        <Image
+                            src='/client1.jpg'
+                            alt='Cliente de Pura Serendipia'
+                            layout='fill'
+                            className={css(ClientsOpinionSectionCardImageStyles)}
+                        />
+                    </ClientsOpinionSectionCardImageContainer>
+                    <LabelLarge>
+                        Muy buena la calidad y la atención al cliente.
+                    </LabelLarge>
+                    <LabelSmall>Claudia Ferraro</LabelSmall>
+                </ClientsOpinionSectionCard>
+                <ClientsOpinionSectionCard>
+                    <ClientsOpinionSectionCardImageContainer>
+                        <Image
+                            src='/client2.jpg'
+                            alt='Cliente de Pura Serendipia'
+                            layout='fill'
+                            className={css(ClientsOpinionSectionCardImageStyles)}
+                        />
+                    </ClientsOpinionSectionCardImageContainer>
+                    <LabelLarge>
+                        Me compré unos dijes de corazones y me encantaron!
+                    </LabelLarge>
+                    <LabelSmall>María Ester Verdún</LabelSmall>
+                </ClientsOpinionSectionCard>
+                <ClientsOpinionSectionCard>
+                    <ClientsOpinionSectionCardImageContainer>
+                        <Image
+                            src='/client3.jpg'
+                            alt='Cliente de Pura Serendipia'
+                            layout='fill'
+                            className={css(ClientsOpinionSectionCardImageStyles)}
+                        />
+                    </ClientsOpinionSectionCardImageContainer>
+                    <LabelLarge>
+                        No esperaba mucho por el precio pero, de verdad, me sorprendieron.
+                    </LabelLarge>
+                    <LabelSmall>Gonzalo Pérez</LabelSmall>
+                </ClientsOpinionSectionCard>
+            </ClientsOpinionSectionCardsContainer>
         </ClientsOpinionSectionContainer>
     );
 }
--- src/pages-content/home/layouts/clients-opinion/ClientsOpinionSection.styles.ts
+++ src/pages-content/home/layouts/clients-opinion/ClientsOpinionSection.styles.ts
@@ -5,6 +5,7 @@ export const ClientsOpinionSectionContainer = themedStyled('section', ({ $theme 
     flexDirection: 'column',
     width: $theme.custom.pageMaxWidth,
     padding: '4rem 0',
+    color: $theme.colors.contentPrimary,
 }));
 
 export const ClientsOpinionSectionHeadingStyles = {
@@ -17,4 +18,44 @@ export const ClientsOpinionSectionSubHeadingStyles = {
     width: '100%',
     textAlign: 'center',
     marginTop: '1rem',
+    fontWeight: '400',
+};
+
+export const ClientsOpinionSectionCardsContainer = themedStyled('div', {
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: '1rem',
+    marginTop: '2rem',
+});
+
+export const ClientsOpinionSectionCard = themedStyled('div', ({ $theme }) => ({
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '1rem',
+    textAlign: 'center',
+    marginTop: '1.5rem',
+    width: '100%',
+    padding: '3rem 1.5rem 1.5rem 1.5rem',
+    backgroundColor: $theme.colors.backgroundSecondary,
+}));
+
+export const ClientsOpinionSectionCardImageContainer = themedStyled(
+    'div',
+    ({ $theme }) => ({
+        position: 'absolute',
+        top: '-1.5rem',
+        height: '3.5rem',
+        width: '3.5rem',
+        borderRadius: '50%',
+        backgroundColor: $theme.colors.backgroundSecondary,
+        border: '4px solid ' + $theme.colors.backgroundSecondary,
+        boxSizing: 'border-box',
+    })
+);
+
+export const ClientsOpinionSectionCardImageStyles = {
+    borderRadius: '50%',
 };
